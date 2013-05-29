47a4c663b8cfa6e189cf0cb0a79666e880419302
LoZo <robert.lozinsek@gmail.com>
2013-05-29 09:07:40 +0200

    funkcija

---
+++ PHPPage.php
@@ -0,0 +1,15 @@
+<?php
+phpinfo();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
