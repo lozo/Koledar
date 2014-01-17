def4fe5b99e49f1417f5ccf1f314d93fca632c03
lozo <robert.lozinsek@gmail.com>
2014-01-17 14:39:15 +0100

    prvi branch
    prvi bracnk da vidim če toto sraje uploda

---
+++ com_drustva/site/drustva.php
@@ -0,0 +1,16 @@
+﻿<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+error_reporting(E_ALL);
+ini_set('display_errors',1);  
+ /*  import joomla controller library */
+jimport('joomla.application.component.controller');  
+// Get an instance of the controller prefixed by Drustva
+$controller = JController::getInstance('Drustva');
+ 
+// Perform the Request task
+$controller->execute(JRequest::getCmd('task'));
+ 
+// Redirect if set by the controller
+$controller->redirect();
+ ?>
